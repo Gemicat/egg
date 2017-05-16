11897525dd7caa7a7f10fac8c9ae02e919b148f4
Gemicat <gemicat@163.com>
2017-05-16 10:00:36 +0800

    egg学习

--- egg-example/config/config.default.js
+++ egg-example/config/config.default.js
@@ -1,3 +1,10 @@
 // config/config.default.js
 // 切记：要改为自己的 key 值
 exports.keys = 'ksdf99usd9aplrjw';
+
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks'
+  }
+};
